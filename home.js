1edc8d9815e5ecf41f1b67466e8660d44b41b4c4
ridu101 <ridu116540@gmail.com>
2025-09-05 00:19:20 +0600

    toggole done

--- home.js
+++ home.js
@@ -1,16 +1,29 @@
+const validNumber = 8801878710317;
+const validPin = 3119;
+
+
 document.getElementById('btn-add')
     .addEventListener('click', function (e) {
         e.preventDefault()
         const bank = document.getElementById('bank').value;
-        const accountNumber = document.getElementById('bank-account-number').value;
+        const accountNumber = parseInt(document.getElementById('bank-account-number').value);
         const amountNumber = parseInt(document.getElementById('add-amount').value);
-        const pinNumber = document.getElementById('pin-number').value;
+        const pinNumber = parseInt(document.getElementById('pin-number').value);
 
+        if (accountNumber != validNumber) {
+            alert('Provide Correct Number')
+            return;
+        }
+        if (pinNumber != validPin) {
+            alert('Provide Correct Pin');
+            return;
+        }
         // accessioning the available balance 
-        const availableBalance= parseInt(document.getElementById('avail-balance').innerText);
+        const availableBalance = parseInt(document.getElementById('avail-balance').innerText);
 
-        const totalBalance=amountNumber+availableBalance;
+        const totalBalance = amountNumber + availableBalance;
 
-        document.getElementById('avail-balance').innerText=totalBalance;
-     
+        document.getElementById('avail-balance').innerText = totalBalance;
+
+
     })
